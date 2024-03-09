7f564682b1accc60debd69363b27aba7a353557c
Narmin <mnermin064@gmail.com>
2024-03-09 17:25:13 +0400

    React introduction

---
+++ lesson9/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Numune from "./Numune";
+import Test from "./Test";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hamiya salam</h1>
+      <p>hello</p>
+      <Test/>
+      <Numune/>
+    </div>
+  );
+}
+
+export default App;
---
+++ lesson9/src/Numune.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Numune = () => {
+  return (
+    <div>Numune</div>
+  )
+}
+
+export default Numune
